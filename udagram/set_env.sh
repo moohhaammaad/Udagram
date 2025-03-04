6d05e2190d820cdb29097564444675f2e6565db5
C004997 <mohelsayed@ejada.com>
2025-03-04 10:13:09 +0200

    set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME="postgres"
-export POSTGRES_PASSWORD="staySafeUdagram"
-export POSTGRES_HOST="udagram-database.c8kruisna0ti.us-east-2.rds.amazonaws.com"
-export POSTGRES_DB="postgres"
-export AWS_BUCKET="arn:aws:s3:::udagram-proj-bucket"
-export AWS_REGION="us-east-2"
-export DATABASE_URL="postgres://postgres:staySafeUdagram@dudagram-database.c8kruisna0ti.us-east-2.rds.amazonaws.com:5432/postgres"
-export AWS_PROFILE="default"
+# export POSTGRES_USERNAME="postgres"
+# export POSTGRES_PASSWORD="staySafeUdagram"
+# export POSTGRES_HOST="udagram-database.c8kruisna0ti.us-east-2.rds.amazonaws.com"
+# export POSTGRES_DB="postgres"
+# export AWS_BUCKET="arn:aws:s3:::udagram-proj-bucket"
+# export AWS_REGION="us-east-2"
+# export DATABASE_URL="postgres://postgres:staySafeUdagram@dudagram-database.c8kruisna0ti.us-east-2.rds.amazonaws.com:5432/postgres"
+# export AWS_PROFILE="default"
+# echo "Postgres Password: '$POSTGRES_PASSWORD'"
 # export JWT_SECRET=mysecretstring
 # export URL=http://localhost:8100
-echo "Postgres Password: '$POSTGRES_PASSWORD'"
